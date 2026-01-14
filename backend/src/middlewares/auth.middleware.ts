7cde3a0763d3f319a9063212f19ee11d56236674
mknabb92-blip <mknabb92@gmail.com>
2026-01-14 05:50:24 -0600

    feat(backend):Add authMiddleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import type {MiddlewareHandler} from "hono";
+import {jwt} from "hono/jwt";
+import dotenv from "dotenv"
+
+export const globalMiddleware: MiddlewareHandler = async (c, next) => {
+    const path = c.req.path;
+    const method = c.req.method.toLocaleLowerCase();
+
+    const publicRoutes = [
+        {method: "post", path: "/api/auth/signup"},
+        {method: "post", path: "/api/auth/login"}
+    ];
+
+    const isPublic = publicRoutes.some(
+        (r) => r.method === method && r.path === path
+    );
+
+    if(isPublic) {
+        return next();
+    }
+
+    return jwt({
+       secret: process .env.JWT_SECRET ||"dev-secret",
+       alg: "HS256"
+    })(c, next);
+};
