b9471b9508afccdfdd14d685a4bfae8efec6d404
mknabb92-blip <mknabb92@gmail.com>
2026-01-19 09:49:05 -0600

    fix:uploads img in Backend

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -2,13 +2,15 @@ import type {MiddlewareHandler} from "hono";
 import {jwt} from "hono/jwt";
 import dotenv from "dotenv"
 
+dotenv.config()
+
 export const globalMiddleware: MiddlewareHandler = async (c, next) => {
     const path = c.req.path;
     const method = c.req.method.toLocaleLowerCase();
 
     const publicRoutes = [
         {method: "post", path: "/api/auth/signup"},
-        {method: "post", path: "/api/auth/login"}
+        {method: "post", path: "/api/auth/login"},
     ];
 
     const isPublic = publicRoutes.some(
@@ -19,8 +21,12 @@ export const globalMiddleware: MiddlewareHandler = async (c, next) => {
         return next();
     }
 
+    if(path.startsWith("/uploads")) {
+        return next();
+    }
+
     return jwt({
-       secret: process .env.JWT_SECRET ||"dev-secret",
+       secret: process.env.JWT_SECRET || "dev-secret",
        alg: "HS256"
     })(c, next);
 };
